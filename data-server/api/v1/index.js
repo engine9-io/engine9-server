2543cd3db7db5ea13a04c81955ef11c31a9e2f9d
Chris Lundberg <chris@frakture.com>
2023-11-06 23:24:25 +0000

    Refactored endpoints

--- data-server/api/v1/index.js
+++ data-server/api/v1/index.js
@@ -27,11 +27,20 @@ function knexConfigForTenant(accountId) {
 }
 
 function getKnexForRequest(req) {
-  const accountId = req.params.account_id;
+  // the account_id comes from authentication step, or in the headers,
+  // NOT the url parameters, steamengine is the default
+  const headerAccountId = req.get('STEAMENGINE_ACCOUNT_ID');
+  const accountId = headerAccountId || 'steamengine';
+
   let knex = knexCache.get(accountId);
 
   if (!knex) {
-    knex = Knex(knexConfigForTenant(accountId));
+    const config = knexConfigForTenant(accountId);
+    if (!config) {
+      if (!headerAccountId) throw new Error('No STEAMENGINE_ACCOUNT_ID header');
+      throw new Error(`Could not find config for account ${headerAccountId}`);
+    }
+    knex = Knex(config);
     knexCache.set(accountId, knex);
   }
 
@@ -53,10 +62,11 @@ router.get('/:object/:id', async (req, res) => {
 });
 router.get('/:object', async (req, res) => {
   const objectName = req.params.object;
+  const { limit = 100, offset = 0 } = req.query;
   const model = modelMap.get(objectName);
   if (!model) return res.status(404).json({ error: `No such model: ${objectName}` });
-  const objects = await model.query(req.knex).limit(100);
-  return res.json(objects);
+  const data = await model.query(req.knex).limit(limit).offset(offset);
+  return res.json({ data });
 });
 
 module.exports = router;
--- data-server/index.js
+++ data-server/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const apiV1 = require('./api/v1');
 
 const app = express();
 const isDevelopment = process.env.NODE_ENV === 'development';
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // support direct calls, mostly for testing
+    const url = new URL(origin);
+    if (url.hostname === 'localhost' || url.hostname.endsWith('steamengine.io')) {
+      return callback(null, true);
+    }
+    return callback(new Error('Invalid CORS domain'));
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 if (isDevelopment) app.set('json spaces', 2); // number of spaces for indentation
-app.use('/:account_id/api/v1', apiV1);
+app.use('/api/v1', apiV1);
 app.get('/ok', (req, res) => res.json({ ok: true, ...(isDevelopment ? { env: 'development' } : {}) }));
 
 const server = app.listen(3001, () => {
