505361d405ac7bb3f35e46c911f9bf3657fdaf08
Chris Lundberg <chris@frakture.com>
2025-01-18 18:52:45 +0000

    ensure schema

--- workers/LocalPluginPersonWorker.js
+++ workers/LocalPluginPersonWorker.js
@@ -56,7 +56,7 @@ const internalMeta = {
 Worker.prototype.ensureSchema = async function () {
   return this.deployStandard();
 };
-Worker.prototype.importPeople.metadata = internalMeta;
+Worker.prototype.ensureSchema.metadata = internalMeta;
 
 Worker.prototype.importPeople = async function (options) {
   return this.internalLoadFromTable({ table: 'person', ...options });
